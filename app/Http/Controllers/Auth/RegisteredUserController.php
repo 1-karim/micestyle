7ff7a0c72490c5226f5bb0250b27402b82af5df4
Benabdallah  Karim <b.karim@bluepenlabs.com>
2022-05-25 04:45:18 +0100

    uncommented role attachement in registration form

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        // $user->attachRole($request->role_id);
+        $user->attachRole($request->role_id);
         event(new Registered($request->password));
 
         Auth::login($user);
